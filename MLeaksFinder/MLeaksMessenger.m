95ad76e425399258f19cf5e8845af0bdfcdfdf11
nitianxiaoshuai <296722587@qq.com>
2025-08-12 11:52:28 +0800

    修改

--- MLeaksFinder/MLeaksMessenger.m
+++ MLeaksFinder/MLeaksMessenger.m
@@ -1,18 +1,13 @@
-/**
- * Tencent is pleased to support the open source community by making MLeaksFinder available.
- *
- * Copyright (C) 2017 THL A29 Limited, a Tencent company. All rights reserved.
- *
- * Licensed under the BSD 3-Clause License (the "License"); you may not use this file except in compliance with the License. You may obtain a copy of the License at
- *
- * https://opensource.org/licenses/BSD-3-Clause
- *
- * Unless required by applicable law or agreed to in writing, software distributed under the License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the specific language governing permissions and limitations under the License.
- */
+//
+//  MLeaksMessenger.m
+//  MLeaksFinder
+//
+//  Created by 佘泽坡 on 7/17/16.
+//  Copyright © 2016 zeposhe. All rights reserved.
+//
 
 #import "MLeaksMessenger.h"
-
-static __weak UIAlertView *alertView;
+#import <UIKit/UIKit.h>
 
 @implementation MLeaksMessenger
 
@@ -22,16 +17,33 @@ static __weak UIAlertView *alertView;
 
 + (void)alertWithTitle:(NSString *)title
                message:(NSString *)message
-              delegate:(id<UIAlertViewDelegate>)delegate
+              delegate:(id)delegate
  additionalButtonTitle:(NSString *)additionalButtonTitle {
-    [alertView dismissWithClickedButtonIndex:0 animated:NO];
-    UIAlertView *alertViewTemp = [[UIAlertView alloc] initWithTitle:title
-                                                            message:message
-                                                           delegate:delegate
-                                                  cancelButtonTitle:@"OK"
-                                                  otherButtonTitles:additionalButtonTitle, nil];
-    [alertViewTemp show];
-    alertView = alertViewTemp;
+    
+    dispatch_async(dispatch_get_main_queue(), ^{
+        UIViewController *topVC = [UIApplication sharedApplication].keyWindow.rootViewController;
+        while (topVC.presentedViewController) {
+            topVC = topVC.presentedViewController;
+        }
+        
+        UIAlertController *alertController = [UIAlertController alertControllerWithTitle:title
+                                                                                 message:message
+                                                                          preferredStyle:UIAlertControllerStyleAlert];
+        
+        UIAlertAction *okAction = [UIAlertAction actionWithTitle:@"OK"
+                                                           style:UIAlertActionStyleDefault
+                                                         handler:nil];
+        [alertController addAction:okAction];
+        
+        if (additionalButtonTitle) {
+            UIAlertAction *additionalAction = [UIAlertAction actionWithTitle:additionalButtonTitle
+                                                                       style:UIAlertActionStyleDefault
+                                                                     handler:nil];
+            [alertController addAction:additionalAction];
+        }
+        
+        [topVC presentViewController:alertController animated:YES completion:nil];
+    });
     
     NSLog(@"%@: %@", title, message);
 }
